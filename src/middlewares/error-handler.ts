6177d5fda07a34c1c7be45797416acdb3f9c9675
murtalaliyu <murtalaliyu10@gmail.com>
2021-11-18 21:52:47 -0500

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -11,6 +11,8 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err); // log all other errors
+  
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }]
   });
